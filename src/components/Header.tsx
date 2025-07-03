bd953200b3496a821a50acbadb6b7738fb96e256
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-07-03 13:36:17 +0300

    Improved quality of The app's logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,16 +7,16 @@ export default function HeaderSection() {
   return (
       <nav className=" sticky top-0 z-50 shadow-2xl bg-blue-100 dark:bg-gray-700 flex justify-around items-center">
           <Link href={'/'} className=" flex justify-between">
-              <Image src={'/images/blendify_logo.png'} alt="blendify_logo" height={40} width={40} className=" rounded-full"/>
+              <Image src={'/images/blendify_logo.png'} alt="blendify_logo" height={40} width={40} className=" rounded-full" quality={100}/>
               <h1 className=" flex font-extrabold text-lg text-blue-600 hover:shadow-purple-500 items-center dark:text-blue-400">
                   Blendify
               </h1>
           </Link>  
           <Link href={'/swap'} className=" text-gray-600 active:text-blue-600 dark:text-slate-100">Swap</Link>
-          <Link href={'/supply'} className=" text-gray-600 active:text-blue-600 dark:text-slate-100">supply</Link>
+          <Link href={'/supply'} className=" text-gray-600 active:text-blue-600 dark:text-slate-100">Supply</Link>
           <Link href={'/borrow'} className=" text-gray-600 active:text-blue-600 dark:text-slate-100">Borrow</Link>
           <Link href={'/stake'} className=" text-gray-600 active:text-blue-600 dark:text-slate-100">Stake</Link>
-          <Link href={'/portfolio'} className=" text-gray-600 active:text-blue-600 dark:text-slate-100">portfolio</Link>
+          <Link href={'/portfolio'} className=" text-gray-600 active:text-blue-600 dark:text-slate-100">Portfolio</Link>
           <ThemeToggle/>
     </nav>
   )
