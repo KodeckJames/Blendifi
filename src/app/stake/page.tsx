1fb442849c34e83664e34d87c8831d01626c02cb
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-07-04 14:25:47 +0300

    Configured the Stake page

--- src/app/stake/page.tsx
+++ src/app/stake/page.tsx
@@ -1,7 +1,38 @@
-import React from 'react'
+'use client'
 
-export default function StakePage() {
+import { Button } from '@/components/ui/button'
+import { Card, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { MagicCard } from '@/components/magicui/magic-card'
+import { useTheme } from 'next-themes'
+import { ShineBorder } from '@/components/magicui/shine-border'
+import SelectToken from '@/components/SelectTokens'
+
+export default function BorrowPage() {
+  const { theme } = useTheme()
   return (
-    <div>StakePage</div>
+    <Card className="p-0 max-w-sm w-full shadow-none border-none">
+      <MagicCard
+        gradientColor={theme === 'dark' ? '#D9D9D955' : '#D9D9D955'}
+        className="p-0"
+      >
+        <ShineBorder
+          shineColor={['#A07CFE', '#FE8FB5', '#FFBE7B']}
+          className=" rounded-xl"
+        />
+
+        <CardHeader className="p-4">
+          <CardTitle className=" font-bold text-xl">Stake bTokens</CardTitle>
+          <br />
+          <CardTitle className=" font-semibold">bToken</CardTitle>
+          <SelectToken />
+          <CardTitle className=" font-semibold">Amount</CardTitle>
+          <Input type="number" min={0} placeholder="Input amount" />
+        </CardHeader>
+        <CardFooter className="p-4 border-t border-border [.border-t]:pt-4">
+          <Button className="w-full bg-blue-600 hover:bg-blue-400">Stake</Button>
+        </CardFooter>
+      </MagicCard>
+    </Card>
   )
 }
